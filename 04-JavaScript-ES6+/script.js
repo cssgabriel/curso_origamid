396df84b8d9cfd42bf38e5d54b1961ba3899a90f
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-08 15:04:11 -0300

    Aula de objeto em JS

--- 04-JavaScript-ES6+/script.js
+++ 04-JavaScript-ES6+/script.js
@@ -1,16 +1,12 @@
-const pessoa = {
-    nome: "Gabriel",
-    sobrenome: "Silva",
-    idade: "24",
-    altura: "176",
-    peso: 75,
-    nomeCompleto(){
-        console.log(`${this.nome} ${this.sobrenome}`);
-    },
+const cachorro = {
+    raca: "Labrador",
+    cor: "preto",
+    idade: 2,
+    late() {
+        if(homem) {
+            console.log("AU AU AU");
+        } else {
+            console.log("...")
+        }
+    }
 }
-
-pessoa.andar = function() {
-    console.log("Funcionou!");
-}
-
-pessoa.altura = 3000;
