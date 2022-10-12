55c5ff8468f5a49e6511ccc3a01d9a9dcab301bf
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-12 15:00:30 -0300

    Treinando JS

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
+++
@@ -1,9 +0,0 @@
-const nome = document.querySelector(".header");
-
-nome.addEventListener("click", function() {
-    console.log("Função anônima executada!")
-});
-
-const firstSibling = nome.nextElementSibling;
-
-const firstParent = nome.parentElement;
