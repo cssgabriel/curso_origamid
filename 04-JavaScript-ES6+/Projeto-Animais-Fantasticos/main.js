bdac699ed1c6ac355fc36544eac1e30c6a0f3602
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-14 20:12:40 -0300

    Fazendo exercício de JS [forEach]

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
@@ -1,9 +1,3 @@
 const p = document.querySelectorAll("p");
 
-p.forEach((item, i) => console.log(item, i));
-
-const pHTML = document.getElementsByTagName("p");
-
-const pNewHTML = Array.from(pHTML);
-
-console.log(pNewHTML);
+p.forEach((item, i) => console.log(item.textContent));
