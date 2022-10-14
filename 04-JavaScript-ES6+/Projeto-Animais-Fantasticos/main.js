fffc4033bd58c44b68b85b93dd11966864253ac9
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-14 19:34:41 -0300

    Fazendo exercício de JS

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
@@ -5,3 +5,7 @@ console.log(imagens)
 const imgs = document.querySelectorAll("img[src^='img/imagem']");
 
 console.log(imgs)
+
+const links = document.querySelectorAll("a[href^='#']");
+
+console.log(links);
