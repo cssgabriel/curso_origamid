a84a64884aa2b6f46eab9bc3e2967bcd3c38568c
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-14 21:02:28 -0300

    Fazendo exercício de JS [attributes e classList]

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
@@ -1,1 +1,16 @@
-const gridSection = document.querySelector(".grid-section");
+const itensMenu = document.querySelectorAll(".menu a");
+
+itensMenu.forEach((item) => item.classList.add("ativo"));
+
+itensMenu.forEach((item, i) => {
+    if(i !== 0) {
+        item.classList.toggle("ativo");
+    }
+});
+
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item) => {
+    console.log(item.hasAttribute("alt"));
+});
+
