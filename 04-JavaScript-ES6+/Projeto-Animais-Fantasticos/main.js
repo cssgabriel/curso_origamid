d68d787aee924a1ccae2d3ae789dd74bea826041
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-17 20:36:17 -0300

    Fazendo exercício de JS [getBoundingClientRect]

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
@@ -3,3 +3,4 @@ const animais = document.querySelector(".animais-lista");
 console.log(animais.clientHeight);
 
 console.log(animais.getBoundingClientRect());
+console.log(animais.getBoundingClientRect());
