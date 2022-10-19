f76cebdcdd6920a4b3cc9078aee3b774ed45279b
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-19 16:37:50 -0300

    Função initSoftScroll JS

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
@@ -45,3 +45,25 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initSoftScroll() {
+
+    const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+    if(linksInternos){
+        function scrollToSection(event) {
+            event.preventDefault();
+            const href = event.currentTarget.getAttribute("href");
+            const section = document.querySelector(href);
+            section.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+
+        linksInternos.forEach((link) => {
+            link.addEventListener("click", scrollToSection);
+        });
+    }
+}
+
+initSoftScroll();
