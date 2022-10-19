f0aa847c8fe1a5f78c53f136bec0bd22dd6a59ae
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-19 16:36:55 -0300

    Função initTabNav JS

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/main.js
@@ -1,9 +1,25 @@
-const linksInternos = document.querySelectorAll("a[href^='#']");
+function initTabNav() {
+        
+    const tabMenu = document.querySelector(".js-tabmenu li");
+    const tabContent = document.querySelector(".js-tabcontent section");
 
-function clicou(event) {
-    console.log(event.currentTarget);
+    if(tabMenu && tabContent) {
+        tabContent[0].classList.add("ativo");
+
+        function activeTab(i) {
+            tabContent.forEach(section => {
+                section.classList.remove("ativo");
+            });
+
+            tabContent[i].classList.add("ativo");
+        }
+
+        tabMenu.forEach((itemMenu, i) => {
+            itemMenu.addEventListener("click", () => {
+                activeTab(i);
+            });
+        });
+    }
 }
 
-linksInternos.forEach((link) => {
-    link.addEventListener("click", clicou);
-});
+initTabNav();
