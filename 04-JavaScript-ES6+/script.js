0042d7a296a5a1b4db46783c5e43cea3636eb0bd
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-20 14:21:39 -0300

    Fazendo exercício de JS [Objetos e Protótipos]

--- 04-JavaScript-ES6+/script.js
+++ 04-JavaScript-ES6+/script.js
@@ -1,25 +1,69 @@
-function Pessoa(nome, idade) {
-    this.nome = nome;
-    this.idade = idade;
-    this.andar = function() {
-        console.log(this.nome + " está andando!")
-    };
-}
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
 
-const joao = new Pessoa("João", 20);
-const maria = new Pessoa("Maria", 25);
-const bruno = new Pessoa("Bruno", 15);
+const transacoes = [
+    {
+      descricao: 'Taxa do Pão',
+      valor: 'R$ 39',
+    },
+    {
+      descricao: 'Taxa do Mercado',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 99',
+    },
+    {
+      descricao: 'Taxa do Banco',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 49',
+    },
+];
 
-function Dom(element) {
-    this.elemento = document.querySelectorAll(element);
-    this.addClass = function(cls) {
-        this.elemento.forEach((item) => {
-            item.classList.add(cls);
-        });
-    };
-    this.removeClass = function(cls){
-        this.elemento.forEach((item) => {
-            item.classList.remove(cls);
-        });
-    };
-}
+let sumTax = 0
+let sumRec = 0
+
+transacoes.forEach((item) => {
+    if(item.descricao.toUpperCase().includes("TAXA")) {
+        sumTax += Number(item.valor.replace("R$ ", ""));
+    } else {
+        sumRec += Number(item.valor.replace("R$ ", ""));
+    }
+});
+
+console.log(sumTax, sumRec)
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  // Retorne uma array com a lista abaixo
+  const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+  
+  // Substitua todos os span's por a's
+  const html = `<ul>
+                  <li><span>Sobre</span></li>
+                  <li><span>Produtos</span></li>
+                  <li><span>Contato</span></li>
+                </ul>`;
+  
+  // Retorne o último caracter da frase
+  const frase = 'Melhor do ano!';
+  
+  // Retorne o total de taxas
+  const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+  
+  
+  
