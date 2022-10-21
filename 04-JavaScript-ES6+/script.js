c847c4f72e9b7d6e13854b8da702e6e35ce7962d
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-21 20:05:36 -0300

    Fazendo exercício de JS [Métodos de array]

--- 04-JavaScript-ES6+/script.js
+++ 04-JavaScript-ES6+/script.js
@@ -1,73 +1,24 @@
-// Utilizando o foreach na array abaixo,
-// some os valores de Taxa e os valores de Recebimento
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
 
-const transacoes = [
-    {
-      descricao: 'Taxa do Pão',
-      valor: 'R$ 39',
-    },
-    {
-      descricao: 'Taxa do Mercado',
-      valor: 'R$ 129',
-    },
-    {
-      descricao: 'Recebimento de Cliente',
-      valor: 'R$ 99',
-    },
-    {
-      descricao: 'Taxa do Banco',
-      valor: 'R$ 129',
-    },
-    {
-      descricao: 'Recebimento de Cliente',
-      valor: 'R$ 49',
-    },
-];
+const cursos = document.querySelectorAll(".curso");
 
-let sumTax = 0
-let sumRec = 0
+const arrayCursos = Array.from(cursos);
 
-transacoes.forEach((item) => {
-    if(item.descricao.toUpperCase().includes("TAXA")) {
-        sumTax += Number(item.valor.replace("R$ ", ""));
-    } else {
-        sumRec += Number(item.valor.replace("R$ ", ""));
-    }
+const retornoCurso = arrayCursos.map((curso) => {
+  const titulo = curso.querySelector("h1").innerText;
+  const descricao = curso.querySelector("p").innerText;
+  const aulas = curso.querySelector(".aulas").innerText;
+  const horas = curso.querySelector(".horas").innerText;
+
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas,
+  }
 });
 
-console.log(sumTax, sumRec)
 
-// Retorne uma array com a lista abaixo
-const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
-
-const arr = transportes.split(";");
-console.log(arr);
-
-
-// Substitua todos os span's por a's
-let html = `
-<ul>
-    <li><span>Sobre</span></li>
-    <li><span>Produtos</span></li>
-    <li><span>Contato</span></li>
-</ul>`;
-
-html = html.split("span").join("a");
-console.log(html);
-
-// Retorne o último caracter da frase
-const frase = 'Melhor do ano!';
-
-console.log(frase.slice(-1));
-
-// Retorne o total de taxas
-const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
-
-let sum = 0
-transacoes2.forEach((item) => {
-    if(item.toUpperCase().includes("TAXA")) {
-        sum++;
-    }
-});
-
-console.log(sum);
+console.log(retornoCurso);
