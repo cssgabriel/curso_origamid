38c1c1c433dc2d859389ca322bbc948d30174538
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-21 20:09:08 -0300

    Fazendo exercício de JS [Object]

--- 04-JavaScript-ES6+/script.js
+++ 04-JavaScript-ES6+/script.js
@@ -1,28 +1,13 @@
-// Retorne a soma total de caracteres dos
-// parágrafos acima utilizando reduce
-const paragrafos = document.querySelectorAll("p");
-const totCaracteres = Array.prototype.reduce.call(paragrafos, (acc,p) => {
-    const caracteres = p.innerText.length;
-    return acc + caracteres;
-}, 0);
-
-console.log(totCaracteres);
-
-// Crie uma função que retorne novos elementos
-// html, com os seguintes parâmetros
-// tag, classe e conteudo.
-
-function createEl(tag,cls,content) {
-    const element = document.createElement(tag);
-    element.classList.add(cls);
-    element.innerHTML = content;
-    return element;
+function verificaDado(dado){
+  return Object.prototype.toString.call(dado);
 }
 
-console.log(createEl("h1", "bg-1","Esse é um título"));
 
-// Crie uma nova função utilizando a anterior como base
-// essa nova função deverá sempre criar h1 com a
-// classe titulo. Porém o parâmetro conteudo continuará dinâmico
+const quadrado = {};
 
-const h1 = createEl.bind(null, "h1","titulo");
+Object.defineProperties(quadrado, {
+  lados: {
+      value: 4,
+      enumerable: true,
+  }
+});
