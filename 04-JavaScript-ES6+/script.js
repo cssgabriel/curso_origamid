d3f378377ae01fc1af0f726e12c003b22fe04990
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-21 20:08:10 -0300

    Fazendo exercício de JS [reduce, map, call, apply, bind]

--- 04-JavaScript-ES6+/script.js
+++ 04-JavaScript-ES6+/script.js
@@ -1,72 +1,28 @@
-// Selecione cada curso e retorne uma array
-// com objetos contendo o título, descricao,
-// aulas e horas de cada curso
-
-const cursos = document.querySelectorAll(".curso");
-
-const arrayCursos = Array.from(cursos);
-
-const retornoCurso = arrayCursos.map((curso) => {
-  const titulo = curso.querySelector("h1").innerText;
-  const descricao = curso.querySelector("p").innerText;
-  const aulas = curso.querySelector(".aulas").innerText;
-  const horas = curso.querySelector(".horas").innerText;
-
-  return {
-    titulo,
-    descricao,
-    aulas,
-    horas,
-  }
-});
-
-
-console.log(retornoCurso);
-
-// Retorne uma lista com os
-// números maiores que 100
-const numeros = [3, 44, 333, 23, 122, 322, 33];
-
-const higher100 = numeros.filter((value) => value > 100);
-console.log(higher100)
-
-// Verifique se Baixo faz parte
-// da lista de instrumentos e retorne true
-const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
-
-const contemBaixo = instrumentos.some((instrumento) => instrumento == "Baixo");
-const contemBaixo2 = instrumentos.includes("Baixo");
-
-console.log(contemBaixo, contemBaixo2)
-
-// Retorne o valor total das compras
-const compras = [
-  {
-    item: 'Banana',
-    preco: 'R$ 4,99'
-  },
-  {
-    item: 'Ovo',
-    preco: 'R$ 2,99'
-  },
-  {
-    item: 'Carne',
-    preco: 'R$ 25,49'
-  },
-  {
-    item: 'Refrigerante',
-    preco: 'R$ 5,35'
-  },
-  {
-    item: 'Quejo',
-    preco: 'R$ 10,60'
-  }
-]
-
-
-const sumValue = compras.reduce((acc, value) => {
-  const formatValue = +value.preco.replace("R$", "").trim().replace(",", ".");
-  return acc + formatValue;
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll("p");
+const totCaracteres = Array.prototype.reduce.call(paragrafos, (acc,p) => {
+    const caracteres = p.innerText.length;
+    return acc + caracteres;
 }, 0);
 
-console.log(sumValue);
+console.log(totCaracteres);
+
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+
+function createEl(tag,cls,content) {
+    const element = document.createElement(tag);
+    element.classList.add(cls);
+    element.innerHTML = content;
+    return element;
+}
+
+console.log(createEl("h1", "bg-1","Esse é um título"));
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
+
+const h1 = createEl.bind(null, "h1","titulo");
