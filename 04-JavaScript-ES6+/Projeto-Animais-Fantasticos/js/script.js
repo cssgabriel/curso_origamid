3af2998da47e9724d6210ebc3a47cce4b33bfc52
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-22 17:24:00 -0300

    Adicionando pop-up botao login]

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/js/script.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/js/script.js
@@ -2,8 +2,10 @@ import initSoftScroll from "./modules/scroll-suave.js";
 import initAnimationScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initSoftScroll();
 initAnimationScroll();
 initAccordion();
 initTabNav();
+initModal();
