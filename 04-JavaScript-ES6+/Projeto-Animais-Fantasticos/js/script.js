233cd6befcb394e3eaa9fc6d7f246fc7c61b436a
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-24 21:41:39 -0300

    Dropdown sub menu

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/js/script.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/js/script.js
@@ -4,10 +4,12 @@ import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 initSoftScroll();
 initAnimationScroll();
 initAccordion();
 initTabNav();
 initModal();
+initDropdownMenu();
 initTooltip();
