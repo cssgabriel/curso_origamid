6862c435d0b12315693b1e5e9b9a8e3201eef862
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-25 10:26:35 -0300

    exercícios setTimout e setInterval

--- 04-JavaScript-ES6+/exercicios/script.js
+++ 04-JavaScript-ES6+/exercicios/script.js
@@ -1,13 +1,17 @@
-function verificaDado(dado){
-  return Object.prototype.toString.call(dado);
+function mudarCor() {
+  const body = document.querySelector("body");
+  const attribute = body.hasAttribute("data-blue");
+
+  for(let i = 0; i < 20 ;i++) {
+      if(attribute) {
+          body.setAttribute("data-blue","");
+          body.style.background = "blue";
+      } else {
+          body.setAttribute("data-red","");
+          body.style.background = "red";
+      }
+      setTimeout(1000);
+  }
 }
 
-
-const quadrado = {};
-
-Object.defineProperties(quadrado, {
-  lados: {
-      value: 4,
-      enumerable: true,
-  }
-});
+mudarCor();
