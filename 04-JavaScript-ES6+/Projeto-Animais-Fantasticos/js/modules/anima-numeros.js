c606f500b2344c5ba75cdd73dc0829303df28f4e
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-26 08:50:36 -0300

    anima numeros

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/js/modules/anima-numeros.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/js/modules/anima-numeros.js
@@ -1,3 +1,16 @@
 export default function initAnimaNumeros() {
     
 }
+
+const numeros = document.querySelectorAll("[data-numero]");
+
+numeros.forEach((numero) => {
+    const total = +numero.innerText;
+    let start = 0;
+    const timer = setInterval(() => {
+        start++;
+        if(start > total) {
+            clearInterval(timer);
+        }
+    }, 50);
+});
