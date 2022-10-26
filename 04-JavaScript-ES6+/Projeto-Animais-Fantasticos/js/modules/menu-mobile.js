480ba8797bc6e543a965047af095cbfdffb415f7
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-26 08:42:25 -0300

    menu-mobile

--- 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/js/modules/menu-mobile.js
+++ 04-JavaScript-ES6+/Projeto-Animais-Fantasticos/js/modules/menu-mobile.js
@@ -14,6 +14,6 @@ export default function initMenuMobile() {
                 menuButton.classList.remove("active");
             });
         }
+        eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
     }
-    eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
 }
