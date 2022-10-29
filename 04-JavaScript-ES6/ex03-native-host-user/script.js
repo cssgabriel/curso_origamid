82f3130abf7b4482c8f96c910c5222845744af2a
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-29 15:43:11 -0300

    Exercício 03 - Native, Host e User

---
+++ 04-JavaScript-ES6/ex03-native-host-user/script.js
@@ -0,0 +1,20 @@
+// Liste 5 objetos nativos
+
+Number
+String
+Function
+Object
+Boolean
+Array
+
+
+// Liste 5 objetos do browser
+
+NodeList
+HTMLCollection
+Document
+Window
+HTMLLIElement
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
