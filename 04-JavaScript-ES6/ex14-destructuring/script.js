8376882187f1e38b603a4ead6b453402dba986b2
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-10-31 21:07:26 -0300

    Exercício 14 - Destructuring

---
+++ 04-JavaScript-ES6/ex14-destructuring/script.js
@@ -0,0 +1,25 @@
+// Extraia o backgroundColor, color e margin do btn
+const btn = document.querySelector('button');
+const btnStyles = getComputedStyle(btn);
+
+const {backgroundColor, color, margin} = btn.style;
+console.log(backgroundColor, color, margin)
+
+
+// Troque os valores das variáveis abaixo
+let cursoAtivo = 'JavaScript';
+let cursoInativo = 'HTML';
+
+[cursoAtivo, cursoInativo] = [cursoInativo, cursoAtivo]
+console.log(cursoAtivo, cursoInativo)
+
+// Corrija o erro abaixo
+const cachorro = {
+  nome: 'Bob',
+  raca: 'Labrador',
+  cor: 'Amarelo'
+}
+
+// Forma errada => const {bobCor: cor} = cachorro;
+
+const {cor: bobCor} = cachorro;
