cb15b9bc48de0a93c29d697deff68898c81d2c3f
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-11-02 15:28:33 -0300

    Exercício 17 - Regular Expression [validando cpfs]

--- 04-JavaScript-ES6/ex17-regexp/script.js
+++ 04-JavaScript-ES6/ex17-regexp/script.js
@@ -1,16 +1,38 @@
-// Crie 4 li's na página
-// Utilizando o for...of
-// adicione uma classe a cada li
+const listaLi = document.querySelectorAll("li");
 
-const listLi = document.querySelectorAll("li");
-for(const li of listLi) {
-  li.classList.add("ativo");
+const listaCpfs = ([...listaLi]) => {
+  return listaLi.map(cpf => cpf.innerText);
 }
 
-// Utilize o for...in para listar
-// todos as propriedades e valores
-// do objeto window
-
-for(const w in window) {
-  console.log(window[w], w);
+const limparCpf = (cpf) => {
+  return cpf.replace(/\D/g, "");
 }
+
+const formatarCpf = (cpf) => {
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
+}
+
+const cpfCompleto = (cpfs) => {
+  return cpfs.map(limparCpf).map(formatarCpf);
+}
+
+
+
+// listaLi.forEach((li) => {
+//   const regexp = /(?:\d{3}[.-\s]?){3}[-.\s]?\d{2}/g
+//   console.log(li.innerText.replace(regexp, "Deu Certo"));
+// });
+
+
+
+/* 
+
+111.111.111-11
+222222222-22
+333-333-333-33
+44444444444
+555 555 555 55
+666666.666-66
+777.777.777.77
+
+*/
