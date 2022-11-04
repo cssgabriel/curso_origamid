d71375692b94d81daa8b4fe4e3d5c79a5b1be877
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-11-04 20:59:54 -0300

    Utilizando módulos NPM [eslint, babel, webpack]

--- 04-JavaScript-ES6/Projeto-Animais-Fantasticos/js/modules/accordion.js
+++ 04-JavaScript-ES6/Projeto-Animais-Fantasticos/js/modules/accordion.js
@@ -6,7 +6,6 @@ export default function initAccordion() {
         accordionList[0].classList.add(activeClass);
         accordionList[0].nextElementSibling.classList.add(activeClass);
 
-
         function activeAccordion() {
             this.classList.toggle(activeClass);
             this.nextElementSibling.classList.toggle(activeClass);
