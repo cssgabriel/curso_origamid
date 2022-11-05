2fd45e58b5a2dacee4ed795bcce41a0f04b01499
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-11-05 14:28:53 -0300

    Utilizando módulos NPM [eslint, babel, webpack]

--- 04-JavaScript-ES6/Projeto-Animais-Fantasticos/js/modules/scroll-suave.js
+++ 04-JavaScript-ES6/Projeto-Animais-Fantasticos/js/modules/scroll-suave.js
@@ -1,17 +1,16 @@
 export default function initSoftScroll() {
-
     const linksInternos = document.querySelectorAll("[data-menu='suave'] a[href^='#']");
-    if(linksInternos){
-        function scrollToSection(event) {
-            event.preventDefault();
-            const href = event.currentTarget.getAttribute("href");
-            const section = document.querySelector(href);
-            section.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-            });
-        }
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute("href");
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    }
 
+    if (linksInternos) {
         linksInternos.forEach((link) => {
             link.addEventListener("click", scrollToSection);
         });
