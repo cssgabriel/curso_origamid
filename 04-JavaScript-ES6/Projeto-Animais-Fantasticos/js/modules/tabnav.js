b9a6bc4ac861868acd703433b8a329802331627a
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-11-05 14:29:46 -0300

    Utilizando módulos NPM [eslint, babel, webpack]

--- 04-JavaScript-ES6/Projeto-Animais-Fantasticos/js/modules/tabnav.js
+++ 04-JavaScript-ES6/Projeto-Animais-Fantasticos/js/modules/tabnav.js
@@ -1,20 +1,18 @@
 export default function initTabNav() {
-        
     const tabMenu = document.querySelectorAll("[data-tab='menu'] li");
     const tabContent = document.querySelectorAll("[data-tab='content'] section");
 
-    if(tabMenu && tabContent) {
+    function activeTab(i) {
+        tabContent.forEach((section) => {
+            section.classList.remove("ativo");
+        });
+
+        const direction = tabContent[i].dataset.anime;
+        tabContent[i].classList.add("ativo", direction);
+    }
+
+    if (tabMenu && tabContent) {
         tabContent[0].classList.add("ativo");
-
-        function activeTab(i) {
-            tabContent.forEach(section => {
-                section.classList.remove("ativo");
-            });
-
-            const direction = tabContent[i].dataset.anime;
-            tabContent[i].classList.add("ativo", direction);
-        }
-
         tabMenu.forEach((itemMenu, i) => {
             itemMenu.addEventListener("click", () => {
                 activeTab(i);
