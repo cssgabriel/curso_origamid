e579fe10cb0a95765d608c05bbf0dcc90a01e19c
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2022-11-05 17:26:35 -0300

    refatorando accordion

--- 04-JavaScript-ES6/Projeto-Animais-Fantasticos/js/script.js
+++ 04-JavaScript-ES6/Projeto-Animais-Fantasticos/js/script.js
@@ -1,6 +1,6 @@
 import SoftScroll from "./modules/scroll-suave.js";
 import initAnimationScroll from "./modules/scroll-animacao.js";
-import initAccordion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -13,8 +13,10 @@ import initFetchBitoin from "./modules/fetch-bitcoin.js";
 const scrollSuave = new SoftScroll("[data-menu='suave'] a[href^='#']");
 scrollSuave.init();
 
+const accordion = new Accordion("[data-anime='accordion'] dt");
+accordion.init();
+
 initAnimationScroll();
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
