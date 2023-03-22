b3e87f17988c0f2f7f6d55ae7adc3a2ecc65ad82
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2023-03-22 17:53:15 -0300

    Ex002 - criando funçao toNumber

---
+++ 08 - TypeScript/ex02/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value) {
+    if (Number(value))
+        return Number(value);
+    else
+        throw "value deve ser um número ou uma string";
+}
+console.log(toNumber("gabriel"));
+console.log(toNumber(28));
+console.log(toNumber("10"));
