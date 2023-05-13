53953ff2d8ed60007aa205ec04918ce1b37a6ee5
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2023-05-12 21:47:16 -0300

    Exercicio 8 - refatorando aula anterior

---
+++ 09-React/Aula-02-Vite/src/ex08/App-ex08.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
+
+const coresArray = ["azul", "roxo", "laranja", "verde", "vermelho", "cinza"];
+
+const App = () => {
+  const [cores, setCores] = React.useState<string[]>([]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  function handleChecked(cor: string) {
+    return cores.includes(cor);
+  }
+
+  return (
+    <form>
+      {coresArray.map((cor) => {
+        return (
+          <label key={cor}>
+            <input
+              type="checkbox"
+              value={cor}
+              checked={handleChecked(cor)}
+              onChange={handleChange}
+            />
+            {cor[0].toUpperCase() + cor.slice(1)}
+          </label>
+        );
+      })}
+    </form>
+  );
+};
+
+export default App;
--- 09-React/Aula-02-Vite/src/main.tsx
+++ 09-React/Aula-02-Vite/src/main.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./ex07/App-ex07";
+import App from "./ex08/App-ex08";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
