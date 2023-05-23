1aacf7a5032a785602eaf0edae7e6a5bafb625c7
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2023-05-23 12:01:02 -0300

    UseContext

--- 09-React/Projeto-Final-Dogs/src/Components/Login/LoginForm.tsx
+++ 09-React/Projeto-Final-Dogs/src/Components/Login/LoginForm.tsx
@@ -1,46 +1,49 @@
-import { FormEvent } from "react";
+import React, { FormEvent } from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
-import { TOKEN_POST, USER_GET } from "../../api";
+import { UserContext } from "../../UserContext";
+import Error from "../Helper/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../Forms/Button.module.css";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-
-  async function getuser(token: string) {
-    const { url, options } = USER_GET(token);
-    const res = await fetch(url, options);
-    const data = await res.json();
-    console.log(data);
-  }
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const res = await fetch(url, options);
-      const data = await res.json();
-      window.localStorage.setItem("token", data.token);
-      getuser(data.token);
+      userLogin(username.value, password.value);
     }
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Entrar</Button>
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className={styles.perdeu} to="/login/perdeu">
+        Perdeu a Senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitulo}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/criar">
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 };
---
+++ 09-React/Projeto-Final-Dogs/src/UserContext.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from "./api";
+import { useNavigate } from "react-router-dom";
+
+export const UserContext = React.createContext<null | unknown>(null);
+
+export const UserStorage = ({ children }) => {
+  const [data, setData] = React.useState(null);
+  const [login, setLogin] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const navigate = useNavigate();
+
+  async function getUser(token: string) {
+    const { url, options } = USER_GET(token);
+    const res = await fetch(url, options);
+    const json = await res.json();
+    setData(json);
+    setLogin(true);
+  }
+
+  async function userLogin(username: string, password: string) {
+    try {
+      setError(null);
+      setLoading(true);
+      const { url, options } = TOKEN_POST({ username, password });
+      const tokenRes = await fetch(url, options);
+      if (!tokenRes) throw new Error(`Error: ${tokenRes.statusText}`);
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem("token", token);
+      await getUser(token);
+    } catch (err) {
+      setError(err.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const userLogout = React.useCallback(
+    async function () {
+      setData(null);
+      setError(null);
+      setLogin(false);
+      setLoading(false);
+      window.localStorage.removeItem("token");
+      navigate("/login");
+    },
+    [navigate]
+  );
+
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem("token");
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const res = await fetch(url, options);
+          if (!res.ok) throw new Error("Token inválido!");
+          await getUser(token);
+          navigate("/conta");
+        } catch (err) {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, [userLogout, navigate]);
+
+  return (
+    <UserContext.Provider
+      value={{ userLogin, data, userLogout, error, loading }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
